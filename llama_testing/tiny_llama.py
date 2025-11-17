e1f054f2414f2eb4dda4ef276045fe17bcac0ce1
Adi Balaji <advaithb@umich.edu>
2025-11-16 20:27:45 -0500

    math verifier class

--- llama_testing/tiny_llama.py
+++ llama_testing/tiny_llama.py
@@ -6,16 +6,18 @@ import torch
 from transformers import pipeline
 
 pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16, device_map="auto")
-
+prompt = "Annie has 8 apples. A friend took 2 apples away. How many apples does Annie have left?"
 # We use the tokenizer's chat template to format each message - see https://huggingface.co/docs/transformers/main/en/chat_templating
 messages = [
     {
         "role": "system",
-        "content": "You are a friendly chatbot who always responds in the style of a pirate",
+        "content": "You are a helpful assistant that answers questions.",
     },
-    {"role": "user", "content": "Hello!"},
+    {"role": "user", "content": prompt},
 ]
 prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
-print(outputs[0]["generated_text"])
+response_text = outputs[0]["generated_text"]
+
+print(response_text.strip())
 
